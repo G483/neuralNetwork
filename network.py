8c9d1da00d49780e6d7f1cb0fd2c8332eaf62491
Gabrijel <gabrijel.web@gmail.com>
2021-08-21 14:10:24 +0200

    add some comments

--- network.py
+++ network.py
@@ -1,3 +1,4 @@
+# This code is suppose to represent a one neuron in a neural network
 inputs = [1, 2, 3, 2.5]
 
 # Example 1
@@ -14,8 +15,11 @@ inputs = [1, 2, 3, 2.5]
 #          inputs[0]*weights2[0] + inputs[1]*weights2[1] + inputs[2]*weights2[2] + inputs[3]*weights2[3] + bias2,
 #          inputs[0]*weights3[0] + inputs[1]*weights3[1] + inputs[2]*weights3[2] + inputs[3]*weights3[3] + bias3]
 
+# Biases offset the input values by using positive numbers
 biases = [2, 3, 0.5];
 
+# Weights have much bigger inpact on the input values by multiplying
+# with both positive and negative values
 weights = [
     [0.2, 0.8, -0.5, 1.0],
     [0.5, -0.91, 0.26, -0.5],
